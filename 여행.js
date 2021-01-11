b4702e9ac3b958a725215827e3fb2745df0ac8a7
woojinhong03 <hongwoojin471@gmail.com>
2021-01-11 15:21:38 +0900

    20210111 js 추가

---
+++ 여행.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    
+    var iconList = document.getElementsByClassName('imgs');
+    
+    iconList[0].style.marginLeft = '20%';
+    
+    for(var i = 1; i < iconList.length; i++){
+        iconList[i].style.marginLeft = '6%';
+    }
+
+} 
+
+
